68ffeb96705bb69b4bc5d3ca7c4b86a2a3e21533
Luca <58145073+luca959@users.noreply.github.com>
2023-11-27 14:27:57 +0100

    Update ChristmasLottery.sol dovrebbe essere finito

--- ChristmasLottery.sol
+++ ChristmasLottery.sol
@@ -7,14 +7,17 @@ contract ChristmasLottery{
     struct Ticket{
         uint256 date;
         string Name;
+        string Surname;
     }
     mapping(address => Ticket) LotteryFeed;
     address owner;
+    address wallet;
     address last;
     address[] writers;
     address[] LotteryPartecipants;
+    address[] ExtractedPartecipants;
     
-    event PersonAdded(uint256 _data, string _name, uint256 indexed _NumOfTickets);
+    event PersonAdded(uint256 _data, string _name, string _surname,uint256 indexed _NumOfTickets);
 
     constructor() {
         last = owner = msg.sender;
@@ -25,13 +28,13 @@ contract ChristmasLottery{
         return (LotteryFeed[last]);
     }
 
-    function GetNumOfTicket() public view returns (uint256  num ){
+    function GetNumOfPartecipants() public view returns (uint256  num ){
         require(msg.sender == owner, "Only Luca can read the number of Tickets bought :)");
         num =writers.length ;
         return (num);
     }
 
-    function GetAllTickets() public view returns (Ticket[] memory) {
+    function GetAllPartecipants() public view returns (Ticket[] memory) {
         require(msg.sender == owner, "Only Luca can read all the tickets :)");
 
         Ticket[] memory messages = new Ticket[](writers.length);
@@ -41,31 +44,41 @@ contract ChristmasLottery{
         }
         return (messages);
     }
+    function removeIndex(uint index) private{   
+        require(index < LotteryPartecipants.length);
+        for(uint256 i = index ; i < LotteryPartecipants.length - 1; i ++)
+        {
+            LotteryPartecipants[i] = LotteryPartecipants[i + 1];
+        }
 
-    function GetAllPartecipants() public view returns (address[] memory) {
-        require(msg.sender == owner, "Only Luca can read all the Partecipants :)");
-        return (LotteryPartecipants);
+        LotteryPartecipants.pop();
     }
-    function ExtractLottery() public view returns(Ticket memory) {
+    function Extraction() public returns (address[] memory) {
         require (LotteryPartecipants.length >= 1, "To extract the winner we must have at least 1 partecipant");
         require(msg.sender == owner, "Only Luca can extract the winners :)");
-        uint randNo= uint (keccak256(abi.encodePacked (msg.sender, block.timestamp)))%5;
-        address wallet=LotteryPartecipants[randNo];
+        uint randNo= uint (keccak256(abi.encodePacked (msg.sender, block.timestamp)))%LotteryPartecipants.length;
+        wallet=LotteryPartecipants[randNo];
+        removeIndex(randNo);
+        return LotteryPartecipants;
+
+    }
+    function Winner() public view returns (Ticket memory) {
+        require(msg.sender == owner, "Only Luca can read all the tickets :)");
         return LotteryFeed[wallet];
     }
 
-    function SellTicket(string memory _name, uint256  _NumOfTickets) public {
+    function SellTicket(string memory _name,string memory _surname, uint256  _NumOfTickets) public {
         require (bytes(_name).length > 0 && bytes(_name).length < 256, "Message cannot be empty and cannot be longer than 256 chars");
         require (_NumOfTickets > 0, "Buyer must buy at least 1 ticket");
         if (LotteryFeed[msg.sender].date == 0)
             writers.push(msg.sender);
         
-        LotteryFeed[msg.sender] = Ticket(block.timestamp, _name);
+        LotteryFeed[msg.sender] = Ticket(block.timestamp, _name,_surname);
         for (uint256 i = 0; i < _NumOfTickets ; i++) {
             LotteryPartecipants.push(msg.sender);
         }
         last = msg.sender;
-        emit PersonAdded(block.timestamp, _name, _NumOfTickets);
+        emit PersonAdded(block.timestamp, _name,_surname, _NumOfTickets);
     }
 
 }
