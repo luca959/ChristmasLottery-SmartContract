8a2ac9833ec95f42a229a3b77cdc58080508321c
Luca <58145073+luca959@users.noreply.github.com>
2024-01-25 14:15:00 +0100

    Update ChristmasLottery.sol

--- ChristmasLottery.sol
+++ ChristmasLottery.sol
@@ -8,21 +8,21 @@ contract ChristmasLottery{
         uint256 date;
         string Name;
         string Surname;
-        string Id;
+        uint256 Id;
     }
-    mapping(address => Ticket) LotteryFeed;
+    mapping(uint256 => Ticket) LotteryFeed;
     address owner;
     address wallet;
-    address last;
+    uint256 winner;
+    uint256 last;
     uint randNo;
     address[] writers;
     address[] LotteryPartecipants;
-    address[] ExtractedPartecipants;
     
     event PersonAdded(uint256 _data, string _name, string _surname,string _id,uint256 indexed _NumOfTickets);
 
     constructor() {
-        last = owner = msg.sender;
+        owner = msg.sender;
     }
 
     function getLastTicketSold() public view returns (Ticket memory) {
@@ -31,7 +31,6 @@ contract ChristmasLottery{
     }
 
     function GetNumOfPartecipants() public view returns (uint256  num ){
-        require(msg.sender == owner, "Only Luca can read the number of Tickets bought :)");
         num =writers.length ;
         return (num);
     }
@@ -42,7 +41,7 @@ contract ChristmasLottery{
         Ticket[] memory messages = new Ticket[](writers.length);
         for (uint256 i = 0; i < writers.length; i++) {
 
-            messages[i]=LotteryFeed[writers[i]];
+            messages[i]=LotteryFeed[i];
         }
         return (messages);
     }
@@ -52,50 +51,51 @@ contract ChristmasLottery{
         {
             LotteryPartecipants[i] = LotteryPartecipants[i + 1];
         }
+        // swap LP[index] with LP[last] and then pop
 
         LotteryPartecipants.pop();
     }
-   // function showExtractNum() public view returns(uint ){ // i use this to check if the rand int is really random
-     //   return randNo; 
-    //}
+ 
     function Extraction() public returns (address[] memory) {
         require (LotteryPartecipants.length >= 1, "To extract the winner we must have at least 1 partecipant");
         require(msg.sender == owner, "Only Luca can extract the winners :)");
         randNo= uint (keccak256(abi.encodePacked (msg.sender, block.timestamp)))%LotteryPartecipants.length;
-        wallet=LotteryPartecipants[randNo];
+        //wallet=LotteryPartecipants[randNo];
+        winner=randNo;
         removeIndex(randNo);
         return LotteryPartecipants;
 
     }
     function Winner() public view returns (Ticket memory) {
         require(msg.sender == owner, "Only Luca can read all the tickets :)");
-        return (LotteryFeed[wallet]);
+
+        return (LotteryFeed[winner]);
     }
 
-    function SellTicket(string memory _name,string memory _surname,string memory _id,uint256  _NumOfTickets) public {
+    function SellTicket(string memory _name,string memory _surname,uint256  _id,uint256  _NumOfTickets) public {
         require (bytes(_name).length > 0 && bytes(_name).length < 256, "Message cannot be empty and cannot be longer than 256 chars");
         require (_NumOfTickets > 0, "Buyer must buy at least 1 ticket");
         require (_NumOfTickets < 99, "Buyer must buy at least 1 ticket");
 
-        if (LotteryFeed[msg.sender].date == 0)
+        if (LotteryFeed[_id].date == 0)
             writers.push(msg.sender);
         
-        LotteryFeed[msg.sender] = Ticket(block.timestamp, _name,_surname,_id);
+        LotteryFeed[_id] = Ticket(block.timestamp, _name,_surname,_id);
         for (uint256 i = 0; i < _NumOfTickets ; i++) {
             LotteryPartecipants.push(msg.sender);
         }
-        last = msg.sender;
+        last = _id;
         emit PersonAdded(block.timestamp, _name,_surname,_id, _NumOfTickets);
     }
-        function closeLottery() public {
-            require(msg.sender == owner, "Only Luca can Close the lottery :)");
-            for (uint256 i = 0; i < writers.length; i++) {
-                delete LotteryFeed[writers[i]];
-            }
-            delete writers;
-            delete LotteryPartecipants;
-            delete ExtractedPartecipants;
+    function closeLottery() public {
+        require(msg.sender == owner, "Only Luca can Close the lottery :)");
 
+        for (uint256 i = 0; i < writers.length; i++) {
+            delete LotteryFeed[i];
+        }
+        delete writers;
+        delete LotteryPartecipants;
     }
+
 
 }
